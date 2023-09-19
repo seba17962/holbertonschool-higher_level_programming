46bbdf5c98cfb9d0974a337b16620aa581996c16
seba17962 <isebsebastiansilva@gmail.com>
2023-09-19 21:45:54 +0000

    add the cases if the numbers are greater or less than zero

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if (number % 10 > 5):
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif (number % 10 < 6):
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+last_digit = str(number)
+last_digit = last_digit[-1]
+if (int(last_digit) > 5 and number > 0):
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif (int(last_digit) < 6 and number > 0):
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+elif (number < 0):
+    print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {number % 10} and is 0")
-    
+    print(f"Last digit of {number} is {last_digit} and is 0")

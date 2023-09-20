d5d0111efd68467545d233cb3e3c391ae7d6935d
seba17962 <isebsebastiansilva@gmail.com>
2023-09-20 16:25:33 +0000

    i will be creazy

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
        char_to_change = ord(str[i])
-       if char_to_change > 96 and char_to_change < 123:
+       if char_to_change >= 97 and char_to_change <= 122:
            char_to_change -= (ord('a') - ord('A'))
            print("{}".format(chr(char_to_change)), \
 end='' if i < len(str) -1  else "\n")

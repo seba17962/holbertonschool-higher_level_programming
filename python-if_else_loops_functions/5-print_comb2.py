055cb07cadcd98d07fd1cc0c0c53c5a7532c34da
seba17962 <isebsebastiansilva@gmail.com>
2023-09-20 15:11:16 +0000

    use zfill

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
     if(number < 99):
-        print("{}, ".format((str(number).zfill(2))), end= '')
+        print("{}, ".format((str(number).zfill(2))), end='')
     else:
         print("{}".format(number))

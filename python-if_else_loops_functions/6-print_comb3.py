c2496ebbb97cd35ec4db8e10ae294689f6466ba9
seba17962 <isebsebastiansilva@gmail.com>
2023-09-20 15:40:03 +0000

    plak

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for first_number in range(10):
+    for second_number in range(first_number, 10):
+        if first_number != second_number:
+            if first_number != 8 and second_number != 9:
+                print("{}{}, ".format(first_number, second_number), end='')
+            elif first_number == 8 and second_number == 9:
+                print("{}{}".format(first_number, second_number))

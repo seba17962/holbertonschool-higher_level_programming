05741bb8f67e4b2433266875e0742bd2085adadc
seba17962 <isebsebastiansilva@gmail.com>
2023-09-20 15:43:40 +0000

    now yes

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for first_number in range(10):
     for second_number in range(first_number, 10):
         if first_number != second_number:
-            if first_number != 8 and second_number != 9:
+            if first_number != 8 or second_number != 9:
                 print("{}{}, ".format(first_number, second_number), end='')
             elif first_number == 8 and second_number == 9:
                 print("{}{}".format(first_number, second_number))

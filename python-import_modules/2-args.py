20606a07022e4a9a0227177761c4842eb576d539
seba17962 <isebsebastiansilva@gmail.com>
2023-09-22 15:38:49 +0000

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if len(argv) == 1:
+    print("{} arguments.".format(len(argv) - 1))
+elif len(argv) == 2:
+    print("{} argument:\n{}: {}".format(len(argv), i, argv[i]))
+elif len(argv) > 2:
+    print("{} arguments:".format(len(argv)))
+    for f in range(1, len(argv)):
+        print("{}: {}".format(f, argv[f]))

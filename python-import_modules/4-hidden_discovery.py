ed2d25c23576e2d4d62e062179229f6f2414482b
seba17962 <isebsebastiansilva@gmail.com>
2023-09-22 18:47:57 +0000

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if _name_ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
+    list = dir(hidden_4)
 
-    filtered_names = [name for name in names if not name.startswith("__")]
-
-    sorted_names = sorted(filtered_names)
-
-    for name in sorted_names:
-        print(name)
+    for name in list:
+        if name[:2] != "__":
+            print(name)

2e64a119defc2b6af11860d1571235c8be2ad52b
seba17962 <isebsebastiansilva@gmail.com>
2023-09-22 15:44:11 +0000

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-if len(argv) == 1:
-    print("{} arguments.".format(len(argv) - 1))
-elif len(argv) == 2:
-    print("{} argument:\n{}: {}".format(len(argv) - 1, len(argv) - 1, argv[1]))
-elif len(argv) > 2:
-    print("{} arguments:".format(len(argv)))
-    for f in range(1, len(argv)):
-        print("{}: {}".format(f, argv[f]))
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} argument:\n{}: {}".format(len(argv) - 1, len(argv) - 1, argv[1]))
+    elif len(argv) > 2:
+        print("{} arguments:".format(len(argv)))
+        for f in range(1, len(argv)):
+            print("{}: {}".format(f, argv[f]))

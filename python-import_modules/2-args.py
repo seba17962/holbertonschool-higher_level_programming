280dd3e5071df8d23a94f53db519a0f4445d0a69
seba17962 <isebsebastiansilva@gmail.com>
2023-09-22 16:03:16 +0000

    crazy

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     from sys import argv
 
-    a = len(argv)
-    if a == 1:
-        print("{} arguments.".format(a - 1))
-    elif a == 2:
-        print("{} argument:".format(a - 1))
-        print("{}: {}".format(a - 1, argv[1]))
-    elif a > 2:
-        print("{} arguments:".format(a))
-        for f in range(1, a):
-            print("{}: {}".format(f, argv[f]))
+    _args = len(argv) - 1
+    if len(argv) == 1:
+        print("{} arguments.".format(_args))
+    elif len(argv) == 2:
+        print("{} argument:".format(_args))
+        print("{}: {}".format(_args, argv[1]))
+    elif len(argv) > 2:
+        print("{} arguments:".format(_args))
+        for i in range(1, len(argv)):
+            print("{}: {}".format((i), argv[i]))

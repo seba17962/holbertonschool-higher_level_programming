b5ceb6da183ec2647c2738589050a27caa3cf940
seba17962 <isebsebastiansilva@gmail.com>
2023-09-23 23:38:48 +0000

    use replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        for i in my_string:
-            if (i == 'C' or i == 'c'):
-                my_string.pop([i])
+        my_string = my_string.replace('c', '').replace('C', '')
     return my_string

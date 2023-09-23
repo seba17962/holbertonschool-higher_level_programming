90ebb709ad0a7e2d1f0b5eb096cb797685c1788b
seba17962 <isebsebastiansilva@gmail.com>
2023-09-23 23:49:35 +0000

    try to create a new string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        my_string = my_string.replace('c', '')
-        my_string = my_string.replace('C', '')
-    return my_string
+    new_string = []
+    for character in my_string:
+        if character != 'c' and character != 'C':
+            new_string.append(character)
+    return ''.join(new_string)

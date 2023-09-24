771b89532eb7f54b6c091edd2c8300bc84f62264
seba17962 <isebsebastiansilva@gmail.com>
2023-09-24 15:41:27 +0000

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def divisible_by_2(my_list=[]):
+    if my_list:
+        for i in my_list:
+            if i % 2 == 0:
+                i = True
+            else:
+                i = False
+    return my_list

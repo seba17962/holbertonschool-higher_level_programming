71065c981553a72ff4fcc9fa3fd4dd37ce89abf8
seba17962 <isebsebastiansilva@gmail.com>
2023-09-24 15:37:02 +0000

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def max_integer(my_list=[]):
+    if my_list:
+        max_int = my_list[0]
+        for number in my_list:
+            if number > max_int:
+                max_int = number
+        return max_int
+    return None

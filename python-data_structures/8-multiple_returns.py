cc1d6e8bc667d09a32da0b6f1547b8b18a15ab4a
seba17962 <isebsebastiansilva@gmail.com>
2023-09-24 15:27:43 +0000

    try none

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
+    if sentence == "":
         length = len(sentence)
         first_char = sentence[0]
         tuple_sentence = (length, first_char)

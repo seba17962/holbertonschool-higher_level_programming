542efd07fa577d7878028bee183c4d4cd5f57011
seba17962 <isebsebastiansilva@gmail.com>
2023-09-24 15:02:20 +0000

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
 
+    first_element = a1 + b1
+    second_element = a2 + b2
+
+    new_tuple = (first_element, second_element)
+    return new_tuple

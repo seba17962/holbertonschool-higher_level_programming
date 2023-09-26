b002738958e6563368ea33decb5e8b379b105f72
seba17962 <isebsebastiansilva@gmail.com>
2023-09-26 14:58:13 +0000

    use keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    diff_elements = set_1.difference(set_2)
-    return diff_elements
+def number_keys(a_dictionary):
+    keys = a_dictionary.keys()
+
+    return len(list(keys))

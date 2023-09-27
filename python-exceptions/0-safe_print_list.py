75084dc1cd8056244be5f0c825beb0710c30b369
seba17962 <isebsebastiansilva@gmail.com>
2023-09-27 23:34:29 +0000

    use try and exept, and unused len

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list:
-        for i in range(len(my_list)):
-            if i < x:
-                print("{}".format(my_list[i]), end='')
-
-        print()
-        return my_list[i]
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count

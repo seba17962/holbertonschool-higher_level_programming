267c6eff10fa77c86cc9b2deb19ca632dd4efa56
seba17962 <isebsebastiansilva@gmail.com>
2023-09-28 01:31:46 +0000

    use TypeError and ValueError to fix all cases when value isent a number

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except TypeError:
+    except (TypeError, ValueError):
         print("{} is not an integer".format(value))
         return False
     return True

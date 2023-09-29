1fd5ba5056abcb5e3fea90ea491bb96b3e7a953b
seba17962 <isebsebastiansilva@gmail.com>
2023-09-29 18:07:57 +0000

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+""" This module define a class Square """
 
+
+class Square():
+    """ This class define a Square """
+    def __init__(self, size):
+        self.__size = size
+    pass

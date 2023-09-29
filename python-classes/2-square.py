798049664832009aed9ea816683c42bb5a2e82a3
seba17962 <isebsebastiansilva@gmail.com>
2023-09-29 18:14:58 +0000

    fix code style

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square():
     """ This class define a Square """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:

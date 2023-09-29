9fba6d9134f053ce8d239f5e838748dce2a1eb77
seba17962 <isebsebastiansilva@gmail.com>
2023-09-29 18:13:50 +0000

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+""" This module define a class Square """
 
+
+class Square():
+    """ This class define a Square """
+    def __init__(self, size = 0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    pass

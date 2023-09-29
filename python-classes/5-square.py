2e5aed67aed9e627f65e9ce4e08bfb5398c3e45b
seba17962 <isebsebastiansilva@gmail.com>
2023-09-29 23:07:02 +0000

    task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
+""" This module define a class Square """
 
+
+class Square():
+    """ This class define a Square """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    pass
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for _ in range(self.__size):
+            for _ in range(self.__size):
+                print("#", end='')
+            print()
+
+    def area(self):
+        return (self.__size ** 2)

dcc7c1ed32a168b0baebd7f50ef90d6829200330
seba17962 <isebsebastiansilva@gmail.com>
2023-10-01 17:25:09 +0000

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+    Module that contains the print_square function
+    and it's tested on 4-print_square.txt
 
+"""
+
+
+def print_square(size):
+    """Function that print a Square using #"""
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        for _ in range(size):
+            print("#", end='')
+        print()

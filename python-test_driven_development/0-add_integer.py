d39f92ca920c4b2ae53d6074b90fdd91e6b5b437
seba17962 <isebsebastiansilva@gmail.com>
2023-10-01 15:22:47 +0000

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module defines an add function
+    and it's tested on /tests folder.
+
+"""
+def add_integer(a, b=98):
+    """
+    Adds a and b just if they're int or floats,
+    """
+    if not (isinstance(a, int) or isinstance(a, float)) or \
+       not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b

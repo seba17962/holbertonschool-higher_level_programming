38e34eedb9883da35dd5a719b498728eb7fef22d
seba17962 <isebsebastiansilva@gmail.com>
2023-10-01 17:33:08 +0000

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""
+    This module contains the text_indentation function
+    that it's tested on tests/5-text_indentation.txt
 
+"""
+
+def text_indentation(text):
+    """Function that replaces . ? and : for \n\n"""
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    new_text = text.replace(". ", ".\n\n")
+    new_text = new_text.replace("? ", "?\n\n")
+    new_text = new_text.replace(": ", ":\n\n")
+    print(new_text, end='')

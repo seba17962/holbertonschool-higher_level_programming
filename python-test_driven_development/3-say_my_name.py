3b1491753bc9c15263e5043038a4bc4de5dc07d1
seba17962 <isebsebastiansilva@gmail.com>
2023-10-01 16:35:30 +0000

    add comments

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+    Module that defines the say_my_name
+    function that prints the first and last name.
+
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the first and the last name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

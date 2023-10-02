d874348c7f310085144ccbc22e7fe3fc04f3d798
seba17962 <isebsebastiansilva@gmail.com>
2023-10-02 17:54:45 +0000

    task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -5,16 +5,8 @@
 class Rectangle:
     """pass"""
     def __init__(self, width=0, height=0):
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):

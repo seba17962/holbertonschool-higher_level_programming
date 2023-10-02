b7b7bea9f2ba3a6f5def779055cff19474e51111
seba17962 <isebsebastiansilva@gmail.com>
2023-10-02 17:27:10 +0000

    task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,2 +1,50 @@
 #!/usr/bin/python3
+"""create a class"""
 
+
+class Rectangle:
+    """pass"""
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        result = ""
+        for _ in range(self.__height):
+            for _ in range(self.__width):
+                result += "#"
+            result += "\n"
+        return result

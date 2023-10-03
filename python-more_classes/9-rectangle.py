aa26d4ee0aac79de43865f4e96c406190754d0dc
seba17962 <isebsebastiansilva@gmail.com>
2023-10-03 13:38:11 +0000

    task 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -55,6 +55,10 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
+    @classmethod
+    def square(cls, size=0):
+        return (cls(size, size))
+
     def area(self):
         return self.__height * self.__width
 
@@ -78,10 +82,6 @@ class Rectangle:
 
     def __repr__(self):
         return (f"Rectangle({self.__width}, {self.__height})")
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
 
     def __del__(self):
         Rectangle.number_of_instances -= 1

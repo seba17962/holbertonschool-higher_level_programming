06e20830e5b0972ec225659f09fa75e26cc349ec
seba17962 <isebsebastiansilva@gmail.com>
2023-10-04 14:39:42 +0000

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+def is_same_class(obj, a_class):
+    """_summary_
 
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if isinstance(type(obj), a_class):
+        return True
+    else:
+        return False

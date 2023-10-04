131e765b7830d663912010a43fe73c2f3804f4de
seba17962 <isebsebastiansilva@gmail.com>
2023-10-04 16:31:47 +0000

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+def is_kind_of_class(obj, a_class):
+    """_summary_
 
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return isinstance(obj, a_class)
+        

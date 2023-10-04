23441d96976848f206a49396adf836029c9da08f
seba17962 <isebsebastiansilva@gmail.com>
2023-10-04 16:40:49 +0000

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 def inherits_from(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     if isinstance(obj, a_class):
         return True
     else:

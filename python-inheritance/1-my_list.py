f26a320dfd2895f814fd2ad36827b29f45a26d39
seba17962 <isebsebastiansilva@gmail.com>
2023-10-04 14:16:58 +0000

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+class MyList(list):
+    """_summary_
 
+    Args:
+        list (_type_): _description_
+    """
+    def print_sorted(self):
+        """_summary_
+        """
+        if isinstance(self, int):
+            print(sorted(self))

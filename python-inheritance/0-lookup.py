f853e105e34b9316f7ba5bd10c9fbd0ed7e6dd92
seba17962 <isebsebastiansilva@gmail.com>
2023-10-04 13:49:18 +0000

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,5 +4,13 @@
 
 
 def lookup(obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     list = dir(obj)
     return list

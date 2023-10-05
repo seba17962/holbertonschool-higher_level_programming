55b10043c19f888d98ac0fb6d8af294b723215bf
seba17962 <isebsebastiansilva@gmail.com>
2023-10-05 13:52:02 +0000

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(BaseGeometry):
         BaseGeometry (_type_): _description_
     """
     def __init__(self, width, height):
-        self.integer_validator("width", self.__width)
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", self.__height)
+        self.integer_validator("height", height)
         self.__height = height

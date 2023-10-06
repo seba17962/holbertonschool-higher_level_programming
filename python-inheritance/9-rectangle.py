bbee77d296c971b1c91c9cdf2922d005817788e0
seba17962 <isebsebastiansilva@gmail.com>
2023-10-06 12:59:42 +0000

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    """_summary_
+
+    Args:
+        BaseGeometry (_type_): _description_
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"

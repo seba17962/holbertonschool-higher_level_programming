1c246b452db69149fcae3c8934e1a0c8fff009e8
seba17962 <isebsebastiansilva@gmail.com>
2023-10-06 13:38:32 +0000

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+"""_summary_
 
+Returns:
+    _type_: _description_
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size

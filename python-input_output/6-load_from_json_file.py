aa976547b78ea9b461c68301a6d8eb12e833a420
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 23:16:22 +0000

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+"""creates an Object from a “JSON file”
 
+Returns:
+    obj:
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """_summary_
+
+    Args:
+        filename (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if filename is not None and filename:
+        with open(filename, 'r', encoding="utf-8") as f:
+            return json.load(f)

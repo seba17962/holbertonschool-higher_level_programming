53461dc4e5c0df7b904034d5f0e674ccd78ee3e8
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 23:58:21 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,8 +11,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if os.path.exists('add_item.json'):
-    with open('add_item.json', 'r') as f:
-        list_args = load_from_json_file(f)
+    list_args = load_from_json_file('add_item.json')
 else:
     list_args = []
 

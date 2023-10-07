051343141dd54b9575114fbf709cd670f64b9443
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 15:46:09 +0000

    return num

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+def write_file(filename="", text=""):
+    
+    if filename is not None and filename:
+        with open(filename, 'w', encoding="utf-8") as f:
+            if text is not None:
+                num_chars = f.write(text)
+            return num_chars

08c8ac4b340499efcd4fab3b9ab95359f713d208
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 15:24:17 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+
+
 def read_file(filename=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)

b9f5998c3f73eb720974e67c08d719c4a9618b0d
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 23:52:59 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""script that adds all
+arguments to a Python list,
+and then save them to a file
+"""
+import json
+import os
+from sys import argv
 
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if os.path.exists('add_item.json'):
+    with open('add_item.json', 'r') as f:
+        list_args = load_from_json_file(f)
+else:
+    list_args = []
+
+for arg in argv[1:]:
+    list_args.append(arg)
+
+save_to_json_file(list_args, 'add_item.json')

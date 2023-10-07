9faaceb55afcfe50f3ee5661d1410223c042e8f7
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 22:40:54 +0000

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -14,5 +14,5 @@ def from_json_string(my_str):
         _type_: _description_
     """
     if my_str is not None and my_str:
-        with open(my_str, "r", encoding="utf-8") as f:
+        with open(my_str, "r") as f:
             return json.load(f)

d22220b2b1aa0e6aee9dce247b8ad2e08a3b2fbc
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 15:52:12 +0000

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 
+
+def append_write(filename="", text=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+    """
+    if filename is not None and filename:
+        with open(filename, 'a', encoding="utf-8") as f:
+            if text is not None:
+                return f.write(text)

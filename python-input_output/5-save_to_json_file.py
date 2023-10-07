747079641fc83b4f3202cf3eba83729a524e29a8
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 23:03:53 +0000

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""writes an Object to a text file, using a JSON representation
+"""
+import json
 
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation
+
+    Args:
+        my_obj (list): obj
+        filename (str): json representation
+    """
+    if filename is not None and filename:
+        with open(filename, 'w', encoding="utf-8") as f:
+            x = json.dump(my_obj, f)

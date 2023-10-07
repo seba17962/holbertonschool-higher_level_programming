9cef6ff1798c0d9d029ee7d95e5ecc08b6ccc66f
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 15:47:10 +0000

    return num

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-
+"""_summary_
+"""
 def write_file(filename="", text=""):
-    
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
     if filename is not None and filename:
         with open(filename, 'w', encoding="utf-8") as f:
             if text is not None:

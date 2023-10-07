51daf6d26a2e39aa70006529c887b07da20b9b2a
seba17962 <isebsebastiansilva@gmail.com>
2023-10-07 15:36:50 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     """
     if filename is not None and filename:
         with open(filename, encoding="utf-8") as f:
-          print(f.read())
+            print(f.read())

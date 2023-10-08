5fd910ba5e6fc0f7197b66423adf74f3f8467762
seba17962 <isebsebastiansilva@gmail.com>
2023-10-08 00:13:38 +0000

    fix codestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -15,14 +15,14 @@ def class_to_json(obj):
     """
     # Get the dictionary of attributes from the object
     obj_dict = obj.__dict__
-    
+
     # Create a new dictionary to store serializable attributes
     json_dict = {}
-    
-    # Filter serializable attributes (lists, dictionaries, strings, integers, and booleans)
+
+    # Filter serializable attributes
+    # (lists, dictionaries, strings, integers, and booleans)
     for key, value in obj_dict.items():
         if isinstance(value, (list, dict, str, int, bool)):
             json_dict[key] = value
-    
-    return json_dict
 
+    return json_dict

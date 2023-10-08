764a2b94f838f7830c7e67dc0be82fee76f1865e
seba17962 <isebsebastiansilva@gmail.com>
2023-10-08 23:34:33 +0000

    task 10

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,2 +1,40 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """_summary_
+
+        Args:
+            first_name (str):
+            last_name (str:
+            age (int):
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representing the Student instance.
+        """
+        if attrs is None:
+            attrs = self.__dict__.keys()
+
+        json_dict = {}
+
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+
+        return json_dict
+
+    def reload_from_json(self, json):
+        for key, value in json:
+            if hasattr(self, key):
+                setattr(self, key, value)

d1ab77fd2bf15cd469617b90dfadabe14b61f998
seba17962 <isebsebastiansilva@gmail.com>
2023-10-08 00:29:17 +0000

    task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 
+
+class Studend:
+    def __init__(self, first_name, last_name, age):
+        """_summary_
+
+        Args:
+            first_name (str):
+            last_name (str:
+            age (int):
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representing the Student instance.
+        """
+        json_dict = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+        return json_dict

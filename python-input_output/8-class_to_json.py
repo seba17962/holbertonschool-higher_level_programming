3a0e23e6e533b54babdf8f2254c452e23ddc8989
seba17962 <isebsebastiansilva@gmail.com>
2023-10-08 00:12:03 +0000

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+"""returns the dictionary description with simple data structure
+"""
+def class_to_json(obj):
+    """
+    Converts an object of a class into a dictionary for JSON serialization.
+
+    Args:
+        obj: An object of a class.
+
+    Returns:
+        dict: A dictionary representing the object in JSON-compatible format.
+    """
+    # Get the dictionary of attributes from the object
+    obj_dict = obj.__dict__
+    
+    # Create a new dictionary to store serializable attributes
+    json_dict = {}
+    
+    # Filter serializable attributes (lists, dictionaries, strings, integers, and booleans)
+    for key, value in obj_dict.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+    
+    return json_dict
 

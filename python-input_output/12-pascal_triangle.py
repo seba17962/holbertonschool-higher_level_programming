14982f69207d1c2010ad1efefd447ad5e9b2a578
seba17962 <isebsebastiansilva@gmail.com>
2023-10-09 14:18:05 +0000

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(0, n):
+        row = []
+        for j in range(0, i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                prev_row = triangle[i - 1]
+                value = prev_row[j - 1] + prev_row[j]
+                row.append(value)
+        triangle.append(row)
+    return triangle

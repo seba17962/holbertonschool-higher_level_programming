5b1d0b5354975590ecb3b03b0726a5a9791b4fd1
seba17962 <isebsebastiansilva@gmail.com>
2023-11-12 15:08:18 +0000

    task 14

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Task 14
 -- computes the score average of all records in the table second_table of the database
-SELECT SUM(score)/COUNT(score)
+SELECT SUM(score)/COUNT(score) as average
 FROM second_table;

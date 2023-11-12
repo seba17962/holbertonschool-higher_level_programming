d08d2f608592a583a6aaa6baa2a97a67f4e71416
seba17962 <isebsebastiansilva@gmail.com>
2023-11-12 14:28:56 +0000

    task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- TASK 9
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+create table second_table(
+    id INT,
+    name varchar(256),
+    score INT);
+INSERT INTO second_table(id, name, score)
+VALUE
+('1', 'John', '10'),
+('2', 'Alex', '3'),
+('3', 'Bob', '14'),
+('4', 'George', '8');

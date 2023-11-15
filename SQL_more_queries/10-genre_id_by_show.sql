cdf6d113983dca737c1edc55f937ee26ecd9d16b
seba17962 <isebsebastiansilva@gmail.com>
2023-11-15 18:07:15 +0000

    task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.genre_id = tv_show_genres.id
+JOIN tv_show_genres ON tv_shows.genre_id = tv_show_genres.title
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC

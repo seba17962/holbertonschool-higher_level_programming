91dd6dc03256c3eefb47f90593461a34217da095
seba17962 <isebsebastiansilva@gmail.com>
2023-11-19 00:21:24 +0000

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Task 14
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.title
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_genres.name ASC;
